5f55529af63b8b978e8dfa87bf824b409b2f875e
zero3growlithe <zero3growlithe@gmail.com>
2026-09-25 10:13:43 +0200

    Fix LINQ imports in settings window

--- src/GamepadKeyboard/UI/SettingsWindow.cs
+++ src/GamepadKeyboard/UI/SettingsWindow.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
